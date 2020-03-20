a5a8aa4bb50334aecd7876f3fea75ac4f5280980
Travis <travrobinson33@gmail.com>
2020-03-20 14:34:09 -0600

    finish project 6

--- js/video.js
+++ js/video.js
@@ -1,8 +1,34 @@
 $(document).ready(function(){
-	$('video').mediaelementplayer({
-		success: function(mediaElement, originalNode, instance) {
-			// do things
-		}
+	const player = new Plyr('video', {
+    
 	});
 
+	const vid = document.querySelector('video');
+	const spans = document.querySelectorAll('p span');
+
+	vid.addEventListener('timeupdate', (event) => {
+  	let currentTime = vid.currentTime;
+  	
+  	for(let i = 0; i < spans.length; i += 1) {
+
+  		if(currentTime > spans[i].dataset.start && currentTime < spans[i].dataset.end ) {
+  			spans[i].style.color = '#ffad61';
+  		} else {
+  			spans[i].style.color = 'black';
+  		}
+
+  	}
+
+	});
+
+	for(let i = 0; i < spans.length; i += 1) {
+		spans[i].addEventListener('click', (event) => {
+			let start = spans[i].dataset.start;
+			vid.currentTime = start;
+		});
+	}
+
+
+
 });
+
